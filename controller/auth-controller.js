3369be490e425e0862b49f314d8f2fc95b9cdc2e
Psaruklily <liliia.psaruk@pickethomes.com>
2021-10-19 13:11:54 +0300

    create login user

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,6 +1,15 @@
+const User = require('../database/models/User');
+const { passwordHelper } = require('../helper');
+const bcrypt = require('bcrypt');
+
 module.exports = {
-    userAuth: (req, res) => {
+    userAuth: async(req, res) => {
         const { email, password } = req.body;
-        res.json('Hello from auth controller');
+
+        User.findOne({ email })
+            .then(user => {
+                if(!user) return res.status(400).json({ msg: 'User not exist' });
+                passwordHelper.compare(res, password, user.password);
+            })
     },
 }
--- helper/password-helper.js
+++ helper/password-helper.js
@@ -2,11 +2,13 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
     hash: (password) => bcrypt.hash(password, 10),
-    compare: async(password, hashPassword) => {
-        const isPasswordEquals = await bcrypt.compare(password, hashPassword);
 
-        if(!isPasswordEquals) {
-            throw new Error('Wrong email or password');
+    compare: (res, password, hashPassword) => bcrypt.compare(password, hashPassword, (err, data) => {
+        if(!data) {
+            return res.status(401).json({ msg: 'Invalid credentials' });
         }
-    }
+        if(data) {
+            return res.status(200).json({ msg: 'Login success' });
+        }
+    }),
 }
